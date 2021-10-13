3172a188694a28103f64c83c196cb971544e98f4
Yuri Pimenov <yuri.pimenov@gmail.com>
2021-10-14 01:06:42 +0300

    Rewrite internals

--- android/src/main/kotlin/eu/proxity/proxity_flutter/ProxityFlutterPlugin.kt
+++ android/src/main/kotlin/eu/proxity/proxity_flutter/ProxityFlutterPlugin.kt
@@ -1,15 +1,13 @@
 package eu.proxity.proxity_flutter
 
-import android.app.Activity
+import android.app.*
 import androidx.annotation.NonNull
 import android.content.Context
 import android.content.Intent
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
 import android.os.Handler
 import android.os.Looper
 import androidx.core.app.NotificationCompat
+import android.util.Log
 import java.util.*
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
@@ -20,151 +18,141 @@ import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.EventChannel.StreamHandler
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
-import io.flutter.embedding.engine.plugins.service.ServiceAware
-import io.flutter.embedding.engine.plugins.service.ServicePluginBinding
 
-import eu.proxity.proxitykit.Client
 
-class ProxityFlutterPlugin: FlutterPlugin, MethodCallHandler, ActivityAware, ServiceAware {
-  private lateinit var channel: MethodChannel
-  private var messages: EventChannel.EventSink? = null
-  private var webhooks: EventChannel.EventSink? = null
+class ProxityFlutterPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
+    private lateinit var channel: MethodChannel
+    private var messages: EventChannel.EventSink? = null
+    private var webhooks: EventChannel.EventSink? = null
 
-  private lateinit var context: Context
-  private lateinit var activity: Activity
+    private lateinit var context: Context
+    private lateinit var activity: Activity
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "eu.proxity")
-    channel.setMethodCallHandler(this)
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "eu.proxity")
+        channel.setMethodCallHandler(this)
 
-    context = flutterPluginBinding.applicationContext
+        context = flutterPluginBinding.applicationContext
 
-    EventChannel(flutterPluginBinding.binaryMessenger, "eu.proxity.messages")
-      .setStreamHandler(object : StreamHandler {
-        override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
-          messages = events
-        }
-        override fun onCancel(arguments: Any?) {}
-      })
+        EventChannel(flutterPluginBinding.binaryMessenger, "eu.proxity.messages")
+            .setStreamHandler(object : StreamHandler {
+                override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
+                    messages = events
+                }
+                override fun onCancel(arguments: Any?) {}
+            })
 
-    EventChannel(flutterPluginBinding.binaryMessenger, "eu.proxity.webhooks")
-      .setStreamHandler(object : StreamHandler {
-        override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
-          webhooks = events
-        }
-        override fun onCancel(arguments: Any?) {}
-      })
-  }
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: MethodChannel.Result) {
-    when (call.method) {
-      "initialize" -> {
-        createNotificationChannel()
-        val notificationIntent = Intent(context, activity::class.java)
-        val pendingIntent = PendingIntent.getActivity(context, 0, notificationIntent,0)
-        val notification = NotificationCompat.Builder(context, "ProxityService")
-          .setContentTitle("Proximity service")
-          .setContentText("Finding beacons near you, dear")
-          //.setSmallIcon(R.drawable.icon_notification_center_wh)
-          .setContentIntent(pendingIntent)
-          .build()
-
-        val apiKey = call.argument<String>("apiKey")
-        if (apiKey == null) {
-          result.success(false)
-          return
-        }
-
-        val deviceId = UUID.fromString(call.argument<String>("deviceId"))
-
-        Client.start(context, notification, apiKey, deviceId)
-        result.success(true)
-      }
-      "location" -> {
-        /*
-        val location : Task<Location>? = Client.getInstance()?.location()
-        if (location == null) {
-          result.success(null)
-        } else {
-          result.success(
-            mapOf(
-              "latitude" to location.latitude,
-              "longitude" to location.longitude,
-              "accuracy" to location.accuracy,
-              "speed" to location.speed,
-              "timestamp" to location.time/1000,
-            )
-          )
-        }
-         */
-        result.success(null)
-      }
-      "start" -> {
-        val client = Client.getInstance()
-        client?.setMessagesCallback { ids ->
-          Handler(Looper.getMainLooper()).post {
-            messages?.success(ids)
-          }
-        }
-        client?.setWebhooksCallback { ids ->
-          Handler(Looper.getMainLooper()).post {
-            webhooks?.success(ids)
-          }
-        }
-      }
-      "webhooks" -> {
-        val ids = call.argument<List<String>>("ids")
-        if (ids == null || ids.isEmpty()) {
-          result.error("Empty ids", null, null)
-          return
-        }
-
-        val data = call.argument<String>("data")
-        Client.getInstance()?.runWebhooks(ids, data)
-      }
-      else -> {
-        result.notImplemented()
-      }
+        EventChannel(flutterPluginBinding.binaryMessenger, "eu.proxity.webhooks")
+            .setStreamHandler(object : StreamHandler {
+                override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
+                    webhooks = events
+                }
+                override fun onCancel(arguments: Any?) {}
+            })
     }
-  }
 
-  private fun createNotificationChannel() {
-    val serviceChannel = NotificationChannel(
-      "ProxityService",
-      "Proxity Service Channel",
-      NotificationManager.IMPORTANCE_DEFAULT
-    )
-    val manager = context.getSystemService(NotificationManager::class.java)
-    manager!!.createNotificationChannel(serviceChannel)
-  }
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: MethodChannel.Result) {
+        when (call.method) {
+            "initialize" -> {
+                createNotificationChannel()
+                val notificationIntent = Intent(context, activity::class.java)
+                val pendingIntent = PendingIntent.getActivity(context, 0, notificationIntent,0)
+                val notification = NotificationCompat.Builder(context, "ProxityService")
+                    .setContentTitle("Proximity service")
+                    .setContentText("Finding beacons near you, dear")
+                    //.setSmallIcon(R.drawable.icon_notification_center_wh)
+                    .setContentIntent(pendingIntent)
+                    .build()
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-    messages = null
-    webhooks = null
-  }
+                val apiKey = call.argument<String>("apiKey")
+                if (apiKey == null) {
+                    result.success(false)
+                    return
+                }
 
-  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-    activity = binding.activity
-  }
+                val deviceId = UUID.fromString(call.argument<String>("deviceId"))
 
-  override fun onDetachedFromActivityForConfigChanges() {
-    TODO("Not yet implemented")
-  }
+                ProxityService.start(context, notification, apiKey, deviceId)
+                result.success(true)
+            }
+            "start" -> {
+                // TODO weak ref?
+                ProxityService.getInstance().proxityClient?.start {
+                    Log.d("Content", it.toString())
+                    Handler(Looper.getMainLooper()).post {
+                        if (it.messages.isNotEmpty()) {
+                            messages?.success(it.messages)
+                        }
+                        if (it.webhooks.isNotEmpty()) {
+                            webhooks?.success(it.webhooks)
+                        }
+                    }
+                }
+            }
+            "webhooks" -> {
+                val ids = call.argument<List<String>>("ids")
+                if (ids == null || ids.isEmpty()) {
+                    result.error("Empty ids", null, null)
+                    return
+                }
 
-  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-    TODO("Not yet implemented")
-  }
+                val data = call.argument<String>("data")
+                ProxityService.getInstance().proxityClient?.runWebhooks(ids, data)
+            }
+            "location" -> {
+                /*
+                val location : Task<Location>? = Client.getInstance()?.location()
+                if (location == null) {
+                  result.success(null)
+                } else {
+                  result.success(
+                    mapOf(
+                      "latitude" to location.latitude,
+                      "longitude" to location.longitude,
+                      "accuracy" to location.accuracy,
+                      "speed" to location.speed,
+                      "timestamp" to location.time/1000,
+                    )
+                  )
+                }
+                 */
+                result.success(null)
+            }
+            else -> {
+                result.notImplemented()
+            }
+        }
+    }
 
-  override fun onDetachedFromActivity() {
-    TODO("Not yet implemented")
-  }
+    private fun createNotificationChannel() {
+        val serviceChannel = NotificationChannel(
+              "ProxityService",
+              "Proxity Service Channel",
+              NotificationManager.IMPORTANCE_DEFAULT
+        )
+        val manager = context.getSystemService(NotificationManager::class.java)
+        manager!!.createNotificationChannel(serviceChannel)
+    }
 
-  override fun onAttachedToService(binding: ServicePluginBinding) {
-    TODO("Not yet implemented")
-  }
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+        messages = null
+        webhooks = null
+    }
 
-  override fun onDetachedFromService() {
-    TODO("Not yet implemented")
-  }
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        activity = binding.activity
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDetachedFromActivity() {
+        TODO("Not yet implemented")
+    }
 }
---
+++ android/src/main/kotlin/eu/proxity/proxity_flutter/ProxityService.kt
@@ -0,0 +1,57 @@
+package eu.proxity.proxity_flutter
+
+import android.app.Notification
+import android.app.Service
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.Intent
+import android.os.IBinder
+import androidx.core.content.ContextCompat.getSystemService
+import android.util.Log
+import java.util.*
+
+import eu.proxity.proxitykit.ProxityClient
+
+class ProxityService: Service() {
+    var proxityClient : ProxityClient? = null
+
+    override fun onCreate() {
+        instance = this
+        super.onCreate()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val apiKey = intent?.getStringExtra("api_key") ?: ""
+        val deviceId = intent?.getStringExtra("device_id") ?: UUID.randomUUID().toString()
+        val id = intent?.getIntExtra("id", 123) ?: 123
+        val notification = intent?.getParcelableExtra<Notification>("notification")
+        startForeground(id, notification)
+
+        proxityClient = ProxityClient.getInstance(this)
+        proxityClient!!.initialize(apiKey, deviceId)
+
+        return START_STICKY
+    }
+
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+
+    companion object {
+        private lateinit var instance: ProxityService
+        fun getInstance() : ProxityService = instance
+
+        fun start(context: Context, notification: Notification, apiKey: String, deviceId: UUID) {
+            val startIntent = Intent(context, ProxityService::class.java)
+            startIntent.putExtra("notification", notification)
+            startIntent.putExtra("api_key", apiKey)
+            startIntent.putExtra("device_id", deviceId.toString())
+            context.startForegroundService(startIntent)
+        }
+
+        fun stop(context: Context) {
+            val stopIntent = Intent(context, ProxityService::class.java)
+            context.stopService(stopIntent)
+        }
+    }
+}
